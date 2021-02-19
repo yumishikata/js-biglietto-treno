e1c4f2ee0ac47e21d9c04be52283706b31a38ee4
Yumi Shikata <yumishika17@gmail.com>
2021-02-19 14:04:19 +0100

    Esercizio finito, magari aggiungo qualcosa

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const scontoUnder = 20;
+const scontoOver = 40;
+const costoKm = 0.21;
+
+var kmUtente;
+var anniUtente;
+var costoBiglietto; 
+var scontoApplicato;
+
+kmUtente = prompt("Quanti km vuole percorrere? ");
+
+anniUtente = prompt("Inserisca la sua età per calcolare un eventuale sconto: ");
+
+costoBiglietto = kmUtente * costoKm;
+scontoApplicato = "0%";
+
+if (anniUtente < 18) {
+    scontoApplicato = scontoUnder + "%";
+    costoBiglietto -= (costoBiglietto * scontoUnder / 100);
+} 
+else if (anniUtente > 65) {
+    scontoApplicato = scontoOver + "%";
+    costoBiglietto -= (costoBiglietto * scontoOver / 100);
+}
+
+costoBiglietto = Math.round(costoBiglietto * 100) / 100;
+
+document.getElementById('my_id').innerHTML = 
+"Le è stato applicato uno sconto del " + scontoApplicato + ", quindi il costo del suo biglietto è: " + costoBiglietto + "€";
+
+
